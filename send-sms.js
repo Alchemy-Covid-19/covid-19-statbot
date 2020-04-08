fbda4b94c65ff613f923bd1af519bd63984959fb
Dannie Schumaker <55268278+DannieSchu@users.noreply.github.com>
2020-04-08 14:42:41 -0700

    Created user model and updated send-sms.js (#16)
    * created user model
    
    * modified scraper test (not working yet)
    
    * update send-sms.js to get phone number from array

--- send-sms.js
+++ send-sms.js
@@ -5,20 +5,30 @@ const authToken = process.env.TWILIO_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const request = require('superagent');
 
+const phoneNumbers = [
+  '9714092051',
+  '6087124895',
+  '9717320703',
+  '6508041083',
+  '5036628396'
+];
 
+// change to heroku url
 const getStats = () => request.get('http://localhost:7890/api/v1/stats');
 
-getStats()
-  .then(res => {
-    return client.messages
-      .create({
-        body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
-        \n New Recovered: ${res.body[0].newRecovered}`,
-        from: '+13094080627',
-        to: '+19717320703'
-      });
-  })
-  .then(message => console.log(message.sid));
+phoneNumbers.forEach(phoneNumber => {
+  getStats()
+    .then(res => {
+      return client.messages
+        .create({
+          body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
+          \n New Recovered: ${res.body[0].newRecovered}`,
+          from: '+13094080627',
+          to: `+1${phoneNumber}`
+        });
+    })
+    .then(message => console.log(message.sid));
+});
 
 
 //create GET route for stats
