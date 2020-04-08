06a27602fb5eb2279a317395b14f0b353eb71ee0
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-08 14:59:09 -0700

    moved dynamic-sms to new port

--- dynamic-sms.js
+++ dynamic-sms.js
@@ -4,6 +4,7 @@ const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const request = require('superagent');
+const express = require('express');
 
 const http = require('http');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
@@ -18,20 +19,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/', (req, res) => {
   const twiml = new MessagingResponse();
 
-  if(req.body.Body == 'United States') {
-    getStats()
-      .then(res => {
-        return client.messages
-          .create({
-            body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
+  const messageContent = getStats()
+    .then(res => {
+      return client.messages
+        .create({
+          body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
         \n New Recovered: ${res.body[0].newRecovered}`,
-            from: '+13094080627',
-            to: '+19717320703'
-          });
-      });
+          from: '+13094080627',
+          to: '+16087124895'
+        });
+    });
+
+  if(req.body.Body == 'United States') {
+    twiml.message(messageContent);
   } else {
     twiml.message(
-      'No Body param match, Twilio sends this in the request to your server.'
+      'No data found for that location.'
     );
   }
 
@@ -39,6 +42,6 @@ app.post('/', (req, res) => {
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
+http.createServer(app).listen(7891, () => {
+  console.log('Express server listening on port 7891');
 });
