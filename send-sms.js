a93ae5af65cca0d2f76bf30e4b43057f95f9e593
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-08 15:34:42 -0700

    create getUsers() function that gets user info from database and sends tests

--- send-sms.js
+++ send-sms.js
@@ -5,30 +5,27 @@ const authToken = process.env.TWILIO_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const request = require('superagent');
 
-const phoneNumbers = [
-  '9714092051',
-  '6087124895',
-  '9717320703',
-  '6508041083',
-  '5036628396'
-];
-
 // change to heroku url
 const getStats = () => request.get('http://localhost:7890/api/v1/stats');
 
-phoneNumbers.forEach(phoneNumber => {
-  getStats()
-    .then(res => {
-      return client.messages
-        .create({
-          body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
-          \n New Recovered: ${res.body[0].newRecovered}`,
-          from: '+13094080627',
-          to: `+1${phoneNumber}`
-        });
-    })
-    .then(message => console.log(message.sid));
-});
+const getUsers = () => request.get('http://localhost:7890/api/v1/users');
+
+getUsers()
+  .then(res => {
+    return res.body.forEach(user => {
+      getStats()
+        .then(res => {
+          return client.messages
+            .create({
+              body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
+              \n New Recovered: ${res.body[0].newRecovered}`,
+              from: '+13094080627',
+              to: `+1${user.phoneNumber}`
+            });
+        })
+        .then(message => console.log(message.sid));
+    });
+  });
 
 
 //create GET route for stats
