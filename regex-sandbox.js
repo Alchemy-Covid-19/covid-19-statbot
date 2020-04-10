637029d561527076a59d7cf01acac71370fd3838
Chelsea Spangler <56610933+CNSpangler@users.noreply.github.com>
2020-04-09 17:01:59 -0700

    Features/dynamic sms IT WORKS! (#29)
    * try dynamic location sms response
    
    * moved dynamic-sms to new port
    
    * test and route for get stats by location, not passing yet
    
    * twiml route successfully sends United States data in response to a received text
    
    * dynamic location route works
    
    * switch Twilio credentials/phone numbers to new account
    
    * fixed phone number
    
    * all routes working in postman
    
    * trying to upsertdocs on scrape
    
    * script now runs scraper and replaces old documents with up to date ones
    
    * add time to dynamic post route
    
    * console log in stats.js
    
    * add urlencoded to app.js
    
    * dynamic route works on sms
    
    * added body-parser to pass tests

--- regex-sandbox.js
+++
@@ -1,6 +0,0 @@
-// extracts the daily change
-const dailyIncreaseRegex = /\+(\d{0,3},)?(\d{3},)?\d{0,3}/g;
-const dailyDecreaseRegex = /\-(\d{0,3},)?(\d{3},)?\d{0,3}/g;
-
-// extracts the daily total
-const dailyTotalRegex = /(?<=\\n)(\d{0,3},)?(\d{3},)?\d{0,3}/g;
