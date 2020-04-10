27ebadf7bdd2f7c927f7e12324c307e9e565796c
Chelsea Spangler <56610933+CNSpangler@users.noreply.github.com>
2020-04-09 19:02:59 -0700

    Chelsea (#32)
    * try dynamic location sms response
    
    * moved dynamic-sms to new port
    
    * test and route for get stats by location, not passing yet
    
    * twiml route successfully sends United States data in response to a received text
    
    * dynamic location route works
    
    * switch Twilio credentials/phone numbers to new account
    
    * fixed phone number
    
    * all routes working in postman
    
    * trying to upsertdocs on scrape
    
    * script now runs scraper and replaces old documents with up to date ones
    
    * add time to dynamic post route
    
    * console log in stats.js
    
    * add urlencoded to app.js
    
    * dynamic route works on sms
    
    * added body-parser to pass tests
    
    * require cors

--- dynamic-sms.js
+++ dynamic-sms.js
@@ -1,38 +1,38 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-const request = require('superagent');
-const express = require('express');
+// const accountSid = process.env.TWILIO_SID;
+// const authToken = process.env.TWILIO_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
+// const request = require('superagent');
+// const express = require('express');
 
-const http = require('http');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const bodyParser = require('body-parser');
-const getStatsByLocation = require('./db/data-helpers');
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+// const http = require('http');
+// const MessagingResponse = require('twilio').twiml.MessagingResponse;
+// const bodyParser = require('body-parser');
+// const getStatsByLocation = require('./db/data-helpers');
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 
-// dummy function to text US data when user texts United States
-app.post('/', (req, res) => {
-  const twiml = new MessagingResponse();
+// // dummy function to text US data when user texts United States
+// app.post('/', (req, res) => {
+//   const twiml = new MessagingResponse();
 
-  const messageContent = getStatsByLocation(req.location)
-    .then(res => {
-      return client.messages
-        .create({
-          body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
-        \n New Recovered: ${res.body[0].newRecovered}`,
-          from: '+13094080627',
-          to: '+16087124895'
-        });
-    });
+//   const messageContent = getStatsByLocation(req.location)
+//     .then(res => {
+//       return client.messages
+//         .create({
+//           body: `New Cases: ${res.body[0].newCases} \n New Deaths: ${res.body[0].newDeaths}
+//         \n New Recovered: ${res.body[0].newRecovered}`,
+//           from: '+13094080627',
+//           to: '+16087124895'
+//         });
+//     });
 
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.end(twiml.toString());
-});
+//   res.writeHead(200, { 'Content-Type': 'text/xml' });
+//   res.end(twiml.toString());
+// });
 
-http.createServer(app).listen(7891, () => {
-  console.log('Express server listening on port 7891');
-});
+// http.createServer(app).listen(7891, () => {
+//   console.log('Express server listening on port 7891');
+// });
