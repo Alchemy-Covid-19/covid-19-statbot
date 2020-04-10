702c6faa90bd8d42bd32a5d7afb50a55f4362665
Chelsea Spangler <56610933+CNSpangler@users.noreply.github.com>
2020-04-10 10:16:32 -0700

    Chelsea (#37)
    * try dynamic location sms response
    
    * moved dynamic-sms to new port
    
    * test and route for get stats by location, not passing yet
    
    * twiml route successfully sends United States data in response to a received text
    
    * dynamic location route works
    
    * switch Twilio credentials/phone numbers to new account
    
    * fixed phone number
    
    * all routes working in postman
    
    * trying to upsertdocs on scrape
    
    * script now runs scraper and replaces old documents with up to date ones
    
    * add time to dynamic post route
    
    * console log in stats.js
    
    * add urlencoded to app.js
    
    * dynamic route works on sms
    
    * added body-parser to pass tests
    
    * require cors
    
    * personalize messages for users and stats routes

--- dynamic-sms.js
+++ dynamic-sms.js
@@ -13,7 +13,6 @@ const getStatsByLocation = require('./db/data-helpers');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 // dummy function to text US data when user texts United States
 app.post('/', (req, res) => {
   const twiml = new MessagingResponse();
@@ -36,8 +35,3 @@ app.post('/', (req, res) => {
 http.createServer(app).listen(7891, () => {
   console.log('Express server listening on port 7891');
 });
-
-
-
-
-//
