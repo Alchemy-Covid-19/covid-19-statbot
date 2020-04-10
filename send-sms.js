d203ff552b7814570bf7870ba784c74951d8578a
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-10 10:04:58 -0700

    personalize messages for users and stats routes

--- send-sms.js
+++ send-sms.js
@@ -14,7 +14,7 @@ getUsers()
           return client.messages
             .create({
               body: `
-              Daily stats for ${user.location} as of 4:30 pm PST today:\nNew Cases: ${res.body.newCases}\nNew Deaths: ${res.body.newDeaths}\nNew Recovered: ${res.body.newRecovered}`,
+              Here's your daily update from Social Distance-Ping!\nCOVID-19 stats for ${user.location} as of 4:30 pm PST today:\nNew Cases: ${res.body.newCases}\nNew Deaths: ${res.body.newDeaths}\nNew Recovered: ${res.body.newRecovered}`,
               from: '+13094080627',
               to: `+1${user.phoneNumber.replace('+1', '')}`
             });
