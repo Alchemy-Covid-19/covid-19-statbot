ab08d4b07bd4b2a732d070932d6d70519f295282
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-11 11:17:50 -0700

    refactor

--- send-sms.js
+++ send-sms.js
@@ -1,24 +1,48 @@
 require('dotenv').config();
+require('./lib/utils/connect')();
 
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-const { getStats, getUsers } = require('./lib/utils/API-services');
+const mongoose = require('mongoose');
+const User = require('./lib/models/User');
+const Stats = require('./lib/models/Stats');
 
-getUsers()
-  .then(res => {
-    return res.body.forEach(user => {
-      getStats(user.location)
-        .then(res => {
-          console.log(res.body);
-          return client.messages
-            .create({
-              body: `
-              Here's your daily update from Social Distance-Ping!\nCOVID-19 stats for ${user.location} as of 4:30 pm PST today:\nNew Cases: ${res.body.newCases}\nNew Deaths: ${res.body.newDeaths}\nNew Recovered: ${res.body.newRecovered}`,
-              from: '+13094080627',
-              to: `+1${user.phoneNumber.replace('+1', '')}`
-            });
-        })
-        .then(message => console.log(message.sid));
-    });
-  });
+
+const template = (user, stat) => `
+Here's your daily update from Social Distance-Ping!
+COVID-19 stats for ${user.location} as of 4:30 pm PST today:
+New Cases: ${stat.newCases}
+New Deaths: ${stat.newDeaths}
+New Recovered: ${stat.newRecovered}`.trim();
+
+// you can user your User model instead of hitting your API here
+Promise.all([
+  User.find(), // find all users
+  Stats.getLatest() // get latest stats
+])
+  .then(([users, stats]) => {
+    // send an sms for each user and wait for all messages to get sent 
+    return Promise.all(users.map(user => {
+      const stat = stats.find(stat => stat.location === user.location);
+      return user.sendSms(template(user, stat));
+    }));
+  })
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => mongoose.connection.close());
+
+// refactored old way
+// make sure to wait for texts te be sent
+
+// getUsers()
+//   .then(users => {
+//     // need to wait for all stats to come back
+//     // can use forEach without promise.all because there
+//     // is no guarantee that all text messages are send before the
+//     // function finishes. It may work for a small amount of users
+//     // but as you gain more users the function may finish before sending
+//     // all users a text message, since you are not waiting for the promises
+//     // to finish
+//     return Promise.all(users.map(user => Promise.all([user, getStats(user.location)])))
+//     })
+//     .then(userStats => Promise.all(userStats.map(([user, stats]) => {
+//       return outgoingSmsHandler(user.to, template(user, stats))
+//     })))
