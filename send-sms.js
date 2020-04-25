b27c71310f9662ba02ee11b99a5047e5bcd4254f
Dannie Schumaker <55268278+DannieSchu@users.noreply.github.com>
2020-04-24 17:03:39 -0700

    Dannie refactor (#48)
    * revise stats tests to include adjusted timestamp field
    
    * clean up phone numbers in User model
    
    * add instance method to User model for sending an sms
    
    * create handler function that creates a twilio message
    
    * update daily sms script to use models instead of API
    
    * update stats scraper test to expect timestamp
    
    * hourly scraper working; removed bulkWrite and changed createdAt to updatedAt
    
    * send-sms modified to hit database directly and send daily sms
    
    * relocate message templates to utility file
    
    * relocate welcome message to templates file
    
    * refactor POST route for on demand stats
    
    * refactor POST route for when user unsubscribes
    
    * refactor POST welcome route to use instance method on user model
    
    * adjust outgoing phone number according to message content
    
    * resolve circular dependency issue for handleStop
    
    * fix typo in comment
    
    * Fix typo in comment
    
    * add DELETE route for users
    
    * fix typo
    
    * remove unused file and commented-out code

--- send-sms.js
+++ send-sms.js
@@ -21,39 +21,3 @@ Promise.all([
   .then(console.log)
   .catch(console.error)
   .finally(() => mongoose.connection.close());
-
-// const accountSid = process.env.TWILIO_SID;
-// const authToken = process.env.TWILIO_TOKEN;
-// const client = require('twilio')(accountSid, authToken);
-// const { getStats, getUsers } = require('./lib/utils/API-services'); 
-// const outgoingSmsHandler = require('./lib/utils/sms-handler');
-
-
-// REFACTORED VERSION THAT DOESN'T HIT API
-// getUsers()
-//   .then(users => {
-//     return Promise.all(users.map(user => Promise.all([user, getStats(user.location)])));
-//   })
-//   .then(userStats => Promise.all(userStats.map(([user, stats]) => {
-//     return outgoingSmsHandler(user.phoneNumber, template(user, stats));
-//   })));
-
-
-// OUR ORIGINAL FUNCTION
-// getUsers()
-//   .then(res => {
-//     return res.body.forEach(user => {
-//       getStats(user.location)
-//         .then(res => {
-//           console.log(res.body);
-//           return client.messages
-//             .create({
-//               body: `
-//               Here's your daily update from Social Distance-Ping!\nCOVID-19 stats for ${user.location} as of 4:30 pm PST today:\nNew Cases: ${res.body.newCases}\nNew Deaths: ${res.body.newDeaths}\nNew Recovered: ${res.body.newRecovered}`,
-//               from: '+13094080627',
-//               to: `+1${user.phoneNumber.replace('+1', '')}`
-//             });
-//         })
-//         .then(message => console.log(message.sid));
-//     });
-//   });
